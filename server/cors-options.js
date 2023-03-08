44a3fd4e3069161c2b33614eeecdfc46ea78d594
ikirienko <ikirienko>
2023-03-09 02:35:14 +0300

    add some cors options

--- server/cors-options.js
+++ server/cors-options.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "https://todo-list-test-task-production.up.railway.app",
+  "https://todo-list-test-task-production-f900.up.railway.app",
+  "https://todo-list-test-task-7egqk3ewz-ikirienko.vercel.app",
 ];
 
 const corsOptions = {
