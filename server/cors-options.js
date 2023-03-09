80ec54bd8198b8ee387684df59a074d0932d5b70
ikirienko <ikirienko>
2023-03-09 04:13:56 +0300

    fix bug in cors options

--- server/cors-options.js
+++ server/cors-options.js
@@ -3,8 +3,8 @@ const allowedOrigins = [
   "http://localhost:3001",
   "https://todo-list-test-task-production.up.railway.app",
   "https://todo-list-test-task-production-f900.up.railway.app",
-  "http://todo-list-test-task-lake.vercel.app",
-  "http://vercel.app",
+  "https://todo-list-test-task-lake.vercel.app",
+  "https://vercel.app",
 ];
 
 const corsOptions = {
